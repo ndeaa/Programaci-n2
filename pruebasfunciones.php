a8da7986143e84e1ae588f2fe73b8e16ac4003fe
Alumno <alumno@isbo.edu.uy>
2024-06-14 20:53:22 -0300

    agregamos la funcion mostrar productos

--- pruebasfunciones.php
+++ pruebasfunciones.php
@@ -11,3 +11,26 @@ function agregarProducto($nombre, $cantidad, $valor, $modelo) {
     ];
     return $producto;
 }
+
+function buscarProductoPorModelo($productos, $modelo) {
+    foreach ($productos as $producto) {
+        if ($producto['modelo'] == $modelo) {
+            return "Nombre: " . $producto['modelo'] . "<br>";
+        }
+    }
+    return "Producto no encontrado.<br>";
+}
+
+function mostrarProductos($productos) {
+    $result = '';
+    foreach ($productos as $producto) {
+        $result .= "Nombre: " . $producto['nombre'] . 
+        ", cantidad: " . $producto['cantidad'] . 
+        "valor: " . $producto['valor'] .
+        "modelo: " . $producto['modelo'] 
+    ."<br>";
+        
+   
+    }
+    return $result;
+}
